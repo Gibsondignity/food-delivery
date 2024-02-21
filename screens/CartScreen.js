24b7f2d47bd238d46bdb446dc4794e76d7d2608a
Gibsondignity <64210963+Gibsondignity@users.noreply.github.com>
2024-02-21 09:03:27 +0000

    cart screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -41,6 +41,7 @@ export default function CartScreen() {
 
         </View>
 
+        {/* Dishes */}
         <ScrollView 
             showsVerticalScrollIndicator={false}
             contentContainerStyle={{
@@ -48,7 +49,28 @@ export default function CartScreen() {
             }}
             className="bg-white pt-5"
         >
-
+            {
+                restaurant.dishes.map((dish, index)=>{
+                    return(
+                        <View key={index}
+                            className="flex-row items-center space-x-3 py-2 px-4 bg-white rounded-3xl mx-2 mb-3 shadow-md"
+                        >
+                            <Text className="font-bold " style={{color: themeColors.text}}>
+                                2 x
+                            </Text>
+                            <Image className="h-14 w-14 rounded-full" source={dish.image}/>
+                            <Text className="flex-1 font-bold text-green-700">{dish.name}</Text>
+                            <Text className="font-semibold text-base">{dish.price}</Text>
+                            <TouchableOpacity
+                                className="p-1 rounded-full"
+                                style={{backgroundColor: themeColors.bgColor(1)}}
+                            >
+                                <Icon.Minus strokeWidth={2} height={20} width={20} stroke="white" />
+                            </TouchableOpacity>
+                        </View>
+                    )
+                })
+            }
         </ScrollView>
 
 
